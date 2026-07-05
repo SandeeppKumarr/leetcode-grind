5a345a82af91b3882910fbc41a02d638c4f96b44
Sandeep Kumar <155826525+SandeeppKumarr@users.noreply.github.com>
2026-07-05 17:57:39 +0530

    Add solution for Set Mismatch

---
+++ Easy/645-set-mismatch.cpp
@@ -0,0 +1,23 @@
+/**
+ * LeetCode #645: Set Mismatch
+ * Difficulty: Easy
+ * Language: C++
+ * Runtime: N/A
+ * Memory: N/A
+ * Date: 2026-07-05T12:27:38.244Z
+ */
+
+class Solution {
+    public int[] findErrorNums(int[] nums) {
+        int xor = 0;
+        int n = nums.length;
+
+        // XOR all array elements and numbers from 1 to n
+        for (int num : nums) {
+            xor ^= num;
+        }
+        for (int i = 1; i <= n; i++) {
+            xor ^= i;
+        }
+
+        // Rightmost set bit
