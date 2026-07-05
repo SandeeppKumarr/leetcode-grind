0de20e1be95c680e6676e86069435c1499bfee99
Sandeep Kumar <155826525+SandeeppKumarr@users.noreply.github.com>
2026-07-05 18:17:18 +0530

    Add solution for Set Mismatch

---
+++ Easy/645-set-mismatch.java
@@ -0,0 +1,52 @@
+/**
+ * LeetCode #645: Set Mismatch
+ * Difficulty: Easy
+ * Language: java
+ * Runtime: N/A
+ * Memory: N/A
+ * Date: 2026-07-05T12:47:16.739Z
+ */
+
+class Solution {
+    public int[] findErrorNums(int[] nums) {
+        int xor = 0;
+        int n = nums.length;
+
+        // XOR all array elements and numbers from 1 to n
+        for (int num : nums) {
+            xor ^= num;
+        }
+        for (int i = 1; i <= n; i++) {
+            xor ^= i;
+        }
+
+        // Rightmost set bit
+        int mask = xor & -xor;
+
+        int x = 0, y = 0;
+
+        // Partition array elements
+        for (int num : nums) {
+            if ((num & mask) != 0)
+                x ^= num;
+            else
+                y ^= num;
+        }
+
+        // Partition numbers 1...n
+        for (int i = 1; i <= n; i++) {
+            if ((i & mask) != 0)
+                x ^= i;
+            else
+                y ^= i;
+        }
+
+        // Determine which is duplicate
+        for (int num : nums) {
+            if (num == x)
+                return new int[]{x, y};
+        }
+
+        return new int[]{y, x};
+    }
+}
