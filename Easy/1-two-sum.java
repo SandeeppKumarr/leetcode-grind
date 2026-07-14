41b2c9531814438fd261aec607fe2515b35475a1
Sandeep Kumar <155826525+SandeeppKumarr@users.noreply.github.com>
2026-07-14 20:47:40 +0530

    Added a solution for Two Sum

--- Easy/1-two-sum.java
+++ Easy/1-two-sum.java
@@ -2,17 +2,16 @@
  * LeetCode #1: Two Sum
  * Difficulty: Easy
  * Language: Java
- * Date: 2026-07-14T15:15:46.041Z
+ * Date: 2026-07-14T15:17:39.635Z
  */
 
 class Solution {
-    public int[] twoSum(int[] numbers, int target) {
-        int l = 0;
-        int r = numbers.length - 1;
-        while(l < r){
-            if((numbers[l] + numbers[r]) == target) return new int[]{l+1, r+1};
-            else if((numbers[l] + numbers[r]) > target) r--;
-            else l++;
+    public int[] twoSum(int[] nums, int target) {
+        HashMap<Integer, Integer> numMap = new HashMap<>();
+        for(int i = 0; i < nums.length; i++){
+            int complement = target - nums[i];
+            if(numMap.containsKey(complement)) return new int[]{numMap.get(complement), i};
+            numMap.put(nums[i], i);
         }
         return new int[]{0,0};
     }
