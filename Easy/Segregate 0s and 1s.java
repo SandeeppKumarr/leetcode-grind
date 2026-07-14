b84cdcff93a52bffc1fd5ac92ef12ed913f7e63e
Sandeep Kumar <155826525+SandeeppKumarr@users.noreply.github.com>
2026-07-15 01:35:51 +0530

    Add solution to segregate 0s and 1s in an array

---
+++ Easy/Segregate 0s and 1s.java
@@ -0,0 +1,30 @@
+/**
+ * GeeksForGeeks: Segregate 0s and 1s
+ * Difficulty: Easy
+ * Language: Java
+ * Date: 2026-07-05T01:35:44.074Z
+ */
+class Solution {
+    void segregate0and1(int[] arr) {
+        // code here
+        int left = 0;
+        int right = arr.length - 1;
+        while(left < right){
+            if((arr[left] == 0) && (arr[right] == 0)) left++;
+            else if((arr[left] == 1) && (arr[right] == 1)) right--;
+            else if((arr[left] == 1) && (arr[right] == 0)){
+                swap(arr, left, right);
+                left++;
+                right--;
+            }
+            else{
+                left++;right--;
+            }
+        }
+    }
+    void swap(int[] arr, int left, int right){
+        int temp = arr[left];
+        arr[left] = arr[right];
+        arr[right] = temp;
+    }
+}
