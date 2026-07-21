4008118307acfdcb7cf99b250c5a74a6c5d99c55
Sandeep Kumar <155826525+SandeeppKumarr@users.noreply.github.com>
2026-07-21 21:16:59 +0530

    Implement maxSubarraySum for size k subarray

---
+++ Easy/gfg-max-sum-subarray-of-size-k.java
@@ -0,0 +1,25 @@
+/**
+ * GeeksForGeeks: Segregate 0s and 1s
+ * Difficulty: Easy
+ * Language: Java
+ * Date: 2026-07-21T21:16:44.074Z
+ */
+class Solution {
+    public int maxSubarraySum(int[] arr, int k) {
+        // Code here
+        int left = 0;
+        int right = left + k - 1;
+        int currSum = 0;
+        for(int i = 0; i < k; i++){
+            currSum += arr[i];
+        }
+        int maxSum = currSum;
+        while(right + 1 < arr.length){
+            currSum = currSum - arr[left] + arr[right + 1];
+            maxSum = Math.max(maxSum, currSum);
+            left++;
+            right++;
+        }
+        return maxSum;
+    }
+}
