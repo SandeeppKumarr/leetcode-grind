0c8be1d5ecbfb2a0a5474b66b2f406b6fa10f4fe
Sandeep Kumar <155826525+SandeeppKumarr@users.noreply.github.com>
2026-07-22 12:17:59 +0530

    Added a solution for Max Consecutive Ones III

--- Medium/1004-max-consecutive-ones-iii.java
+++ Medium/1004-max-consecutive-ones-iii.java
@@ -2,20 +2,18 @@
  * LeetCode #1004: Max Consecutive Ones III
  * Difficulty: Medium
  * Language: Java
- * Date: 2026-07-22T06:45:22.109Z
+ * Date: 2026-07-22T06:47:55.697Z
  */
 
 class Solution {
     public int longestOnes(int[] nums, int k) {
         int maxLength = 0;
         int left = 0;
-        int[] frequency = new int[2];
-        int min = 0;
+        int count = 0;
         for(int right = 0; right < nums.length; right++){
-            frequency[nums[right]]++;
-            min = Math.min(frequency[0], frequency[1]);
-            if(min > k){
-                frequency[nums[left]]--;
+            if(nums[right] == 0) count++;
+            if(count > k){
+                if(nums[left] == 0) count--;
                 left++;
             }
             else maxLength = Math.max(maxLength, right - left + 1);
