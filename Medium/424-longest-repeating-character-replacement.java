9124fde3c1761526e0ba2a252339d2414ecdc1b0
Sandeep Kumar <155826525+SandeeppKumarr@users.noreply.github.com>
2026-07-22 12:22:01 +0530

    Added a solution for Longest Repeating Character Replacement

--- Medium/424-longest-repeating-character-replacement.java
+++ Medium/424-longest-repeating-character-replacement.java
@@ -2,21 +2,21 @@
  * LeetCode #424: Longest Repeating Character Replacement
  * Difficulty: Medium
  * Language: Java
- * Date: 2026-07-22T06:50:05.619Z
+ * Date: 2026-07-22T06:51:57.049Z
  */
 
 class Solution {
     public int characterReplacement(String s, int k) {
-        int maxLength = 0;
+        int[] freq = new int[26];
         int left = 0;
-        int maxCount = 0;
-        Map<Character, Integer> chars = new HashMap<>();
-        for(int right = 0; right < s.length(); right ++){
-            chars.put(s.charAt(right), chars.getOrDefault(s.charAt(right), 0) + 1);
-            maxCount = Math.max(maxCount, chars.get(s.charAt(right)));
-            while((right - left + 1 - maxCount) > k){
-                chars.put(s.charAt(left), chars.get(s.charAt(left)) - 1);
-                if(chars.get(s.charAt(left)) == 0) chars.remove(s.charAt(left));
+        int maxFreq = 0;
+        int maxLength = 0;
+        for (int right = 0; right < s.length(); right++) {
+            int idx = s.charAt(right) - 'A';
+            freq[idx]++;
+            maxFreq = Math.max(maxFreq, freq[idx]);
+            while ((right - left + 1) - maxFreq > k) {
+                freq[s.charAt(left) - 'A']--;
                 left++;
             }
             maxLength = Math.max(maxLength, right - left + 1);
