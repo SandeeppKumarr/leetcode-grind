2710c56beaadc262971b225724ca3f36c39a79f9
Sandeep Kumar <155826525+SandeeppKumarr@users.noreply.github.com>
2026-07-22 12:20:09 +0530

    Added a solution for Longest Repeating Character Replacement

--- Medium/424-longest-repeating-character-replacement.java
+++ Medium/424-longest-repeating-character-replacement.java
@@ -2,7 +2,7 @@
  * LeetCode #424: Longest Repeating Character Replacement
  * Difficulty: Medium
  * Language: Java
- * Date: 2026-07-22T06:32:05.034Z
+ * Date: 2026-07-22T06:50:05.619Z
  */
 
 class Solution {
@@ -14,12 +14,12 @@ class Solution {
         for(int right = 0; right < s.length(); right ++){
             chars.put(s.charAt(right), chars.getOrDefault(s.charAt(right), 0) + 1);
             maxCount = Math.max(maxCount, chars.get(s.charAt(right)));
-            if((right - left + 1 - maxCount) > k){
+            while((right - left + 1 - maxCount) > k){
                 chars.put(s.charAt(left), chars.get(s.charAt(left)) - 1);
                 if(chars.get(s.charAt(left)) == 0) chars.remove(s.charAt(left));
                 left++;
             }
-            else maxLength = Math.max(maxLength, right - left + 1);
+            maxLength = Math.max(maxLength, right - left + 1);
         }
         return maxLength;
     }
