3fdcc238b09db8a3fd80d072a21b591698cfe5de
Sandeep Kumar <155826525+SandeeppKumarr@users.noreply.github.com>
2026-07-22 18:54:59 +0530

    Added a solution for Minimum Window Substring

---
+++ Hard/76-minimum-window-substring.java
@@ -0,0 +1,35 @@
+/**
+ * LeetCode #76: Minimum Window Substring
+ * Difficulty: Hard
+ * Language: Java
+ * Date: 2026-07-22T13:24:55.389Z
+ */
+
+class Solution {
+    public String minWindow(String s, String t) {
+        if (s == null || t == null || s.length() == 0 || t.length() == 0 ||
+                s.length() < t.length()) {
+            return new String();
+        }
+        int[] map = new int[128];
+        int count = t.length();
+        int left = 0, minLen = Integer.MAX_VALUE, startIndex = 0;
+        for(char c : t.toCharArray()){
+            map[c]++;
+        }
+
+        char[] chS = s.toCharArray();
+
+        for(int right = 0; right < chS.length; right++){
+            if(map[chS[right]]-- > 0) count--;
+            while(count == 0){
+                if((right - left + 1) < minLen){
+                    startIndex = left;
+                    minLen = right - left + 1;
+                }
+                if(map[chS[left++]]++ == 0) count++;
+            }
+        }
+        return minLen == Integer.MAX_VALUE ? new String() : new String(chS, startIndex, minLen);
+    }
+}
