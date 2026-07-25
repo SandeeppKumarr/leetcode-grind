bd9cd3ed28f8e94e1de3afb5883b1a7e52bc1b10
Sandeep Kumar <155826525+SandeeppKumarr@users.noreply.github.com>
2026-07-25 18:59:49 +0530

    Added a solution for Happy Number

---
+++ Easy/202-happy-number.java
@@ -0,0 +1,28 @@
+/**
+ * LeetCode #202: Happy Number
+ * Difficulty: Easy
+ * Language: Java
+ * Date: 2026-07-25T13:29:48.388Z
+ */
+
+class Solution {
+    int calc(int n){
+        int sum = 0;
+        while(n > 0){
+            int digit = n % 10;
+            sum += (digit * digit);
+            n /= 10;
+        }
+        return sum;
+    }
+    public boolean isHappy(int n) {
+        int slow = n, fast = n;
+        while(fast != 1 && calc(fast) != 1){
+            slow = calc(slow);
+            fast = calc(calc(fast));
+            //System.out.println(fast);
+            if(slow == fast) return false;
+        }
+        return true;
+    }
+}
