c7caccb03473dc0d9ee19cc05e49aec4771cd892
Sandeep Kumar <155826525+SandeeppKumarr@users.noreply.github.com>
2026-07-25 22:58:06 +0530

    Added a solution for Maximum Product Subarray

--- Medium/152-maximum-product-subarray.java
+++ Medium/152-maximum-product-subarray.java
@@ -2,19 +2,19 @@
  * LeetCode #152: Maximum Product Subarray
  * Difficulty: Medium
  * Language: Java
- * Date: 2026-07-25T17:23:57.797Z
+ * Date: 2026-07-25T17:28:04.821Z
  */
 
 class Solution {
     public int maxProduct(int[] nums) {
-        int currProduct = nums[0];
+        int maxCurrProduct = nums[0];
+        int minCurrProduct = nums[0];
         int maxProduct = nums[0];
-        int minProduct = nums[0];
         for(int i = 1; i < nums.length; i++){
-            int temp = currProduct;
-            currProduct = Math.max(nums[i], Math.max(currProduct * nums[i], minProduct * nums[i]));
-            minProduct = Math.min(nums[i], Math.min(temp * nums[i], minProduct * nums[i]));
-            maxProduct = Math.max(maxProduct, currProduct);
+            int temp = maxCurrProduct;
+            maxCurrProduct = Math.max(nums[i], Math.max(maxCurrProduct * nums[i], minCurrProduct * nums[i]));
+            minCurrProduct = Math.min(nums[i], Math.min(temp * nums[i], minCurrProduct * nums[i]));
+            maxProduct = Math.max(maxProduct, maxCurrProduct);
         }
         return maxProduct;
     }
