d892e3f0723bb8fc4444bc92b2dd491b31d15358
Sandeep Kumar <155826525+SandeeppKumarr@users.noreply.github.com>
2026-07-26 01:32:43 +0530

    Added a solution for Maximum Subarray Sum with One Deletion

--- Medium/1186-maximum-subarray-sum-with-one-deletion.java
+++ Medium/1186-maximum-subarray-sum-with-one-deletion.java
@@ -2,24 +2,19 @@
  * LeetCode #1186: Maximum Subarray Sum with One Deletion
  * Difficulty: Medium
  * Language: Java
- * Date: 2026-07-25T20:01:36.894Z
+ * Date: 2026-07-25T20:02:42.514Z
  */
 
 class Solution {
     public int maximumSum(int[] arr) {
-        int keep = arr[0], deleted = Integer.MIN_VALUE, result = arr[0];
+        long keep = arr[0], deleted = Integer.MIN_VALUE, result = arr[0];
         for(int i = 1; i < arr.length; i++){
-            int prevKeep = keep;
-            int prevDeleted = deleted;
-            deleted = Math.max(
-                prevKeep,
-                prevDeleted == Integer.MIN_VALUE
-                    ? Integer.MIN_VALUE
-                    : prevDeleted + arr[i]
-            );
+            long prevKeep = keep;
+            long prevDeleted = deleted;
+            deleted = Math.max(prevKeep, prevDeleted + arr[i]);
             keep = Math.max(arr[i], prevKeep + arr[i]);
             result = Math.max(result, Math.max(keep, deleted));
         }
-        return result;
+        return (int)result;
     }
 }
