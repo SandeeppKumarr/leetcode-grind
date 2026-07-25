28e8fce10e85331a037ca7e19d8a5d4bb510ea0a
Sandeep Kumar <155826525+SandeeppKumarr@users.noreply.github.com>
2026-07-25 19:33:50 +0530

    Added a solution for Maximum Subarray

--- Medium/53-maximum-subarray.java
+++ Medium/53-maximum-subarray.java
@@ -2,17 +2,18 @@
  * LeetCode #53: Maximum Subarray
  * Difficulty: Medium
  * Language: Java
- * Date: 2026-07-08T17:34:35.664Z
+ * Date: 2026-07-25T14:03:48.932Z
  */
 
 class Solution {
     public int maxSubArray(int[] nums) {
-        int maxSum = nums[0];
         int currSum = nums[0];
+        int maxSum = nums[0];
         for(int i = 1; i < nums.length; i++){
-            currSum = Math.max(nums[i], currSum + nums[i]);
-            maxSum = Math.max(maxSum, currSum); 
+            currSum = Math.max(currSum + nums[i], nums[i]);
+            maxSum = Math.max(maxSum, currSum);
         }
         return maxSum;
     }
 }
+
