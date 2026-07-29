0a2584cea95dfce37e1ca4b5164b91b5bff0cebf
Sandeep Kumar <155826525+SandeeppKumarr@users.noreply.github.com>
2026-07-29 17:30:21 +0530

    Added a solution for Find Pivot Index

---
+++ Easy/724-find-pivot-index.java
@@ -0,0 +1,23 @@
+/**
+ * LeetCode #724: Find Pivot Index
+ * Difficulty: Easy
+ * Language: Java
+ * Date: 2026-07-29T12:00:19.740Z
+ */
+
+class Solution {
+    public int pivotIndex(int[] nums) {
+        int[] prefixSum = new int[nums.length];
+        int[] suffixSum = new int[nums.length];
+        for(int i = 1; i < nums.length; i++){
+            prefixSum[i] = prefixSum[i - 1] + nums[i - 1];
+        }
+        for(int i = nums.length - 2; i > -1; i--){
+            suffixSum[i] = suffixSum[i + 1] + nums[i + 1];
+        }
+        for(int i = 0; i < nums.length; i++){
+            if(prefixSum[i] == suffixSum[i]) return i;
+        }
+        return -1;
+    }
+}
