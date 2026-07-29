192e50f0c302b66e4110116f227f0a54f202d8fb
Sandeep Kumar <155826525+SandeeppKumarr@users.noreply.github.com>
2026-07-30 01:54:17 +0530

    Added a solution for Merge Intervals

--- Medium/56-merge-intervals.java
+++ Medium/56-merge-intervals.java
@@ -2,40 +2,28 @@
  * LeetCode #56: Merge Intervals
  * Difficulty: Medium
  * Language: Java
- * Date: 2026-07-29T20:16:56.358Z
+ * Date: 2026-07-29T20:24:16.048Z
  */
 
 class Solution {
     public int[][] insert(int[][] intervals, int[] newInterval) {
-        int[][] inserted = new int[intervals.length + 1][2];
-        boolean insert = false;
+        List<int[]> res = new ArrayList<>();
 
-        int j = 0;
-        for (int i = 0; i < inserted.length; i++) {
-            if (!insert && (j == intervals.length || intervals[j][0] > newInterval[0])) {
-                inserted[i][0] = newInterval[0];
-                inserted[i][1] = newInterval[1];
-                insert = true;
-            } else {
-                inserted[i][0] = intervals[j][0];
-                inserted[i][1] = intervals[j][1];
-                j++;
+        for (int[] current : intervals) {
+            if (current[1] < newInterval[0]) {
+                res.add(current);
+            }
+            else if (current[0] > newInterval[1]) {
+                res.add(newInterval);
+                newInterval = current;
+            }
+            else {
+                newInterval[0] = Math.min(newInterval[0], current[0]);
+                newInterval[1] = Math.max(newInterval[1], current[1]);
             }
         }
+        res.add(newInterval);
 
-        List<int[]> merged = new ArrayList<>();
-        int[] prev = inserted[0];
-
-        for (int i = 1; i < inserted.length; i++) {
-            if (inserted[i][0] <= prev[1]) {
-                prev[1] = Math.max(prev[1], inserted[i][1]);
-            } else {
-                merged.add(prev);
-                prev = inserted[i];
-            }
-        }
-
-        merged.add(prev);
-        return merged.toArray(new int[merged.size()][]);
+        return res.toArray(new int[res.size()][]);
     }
 }
