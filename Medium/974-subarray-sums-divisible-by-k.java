1c96271881f384c92c24b8cf19ea225c29b4dbcd
Sandeep Kumar <155826525+SandeeppKumarr@users.noreply.github.com>
2026-07-29 20:21:46 +0530

    Added a solution for Subarray Sums Divisible by K

--- Medium/974-subarray-sums-divisible-by-k.java
+++ Medium/974-subarray-sums-divisible-by-k.java
@@ -2,7 +2,7 @@
  * LeetCode #974: Subarray Sums Divisible by K
  * Difficulty: Medium
  * Language: Java
- * Date: 2026-07-29T14:46:40.560Z
+ * Date: 2026-07-29T14:51:45.455Z
  */
 
 class Solution {
@@ -13,8 +13,9 @@ class Solution {
         freq.put(0, 1);
         for(int num : nums){
             prefixSum += num;
-            int mod = prefixSum % k;
-            if(mod < 0) mod += k;
+            //int mod = prefixSum % k;
+            int mod = (prefixSum % k + k) % k;
+            //if(mod < 0) mod += k;
             count += freq.getOrDefault(mod, 0);
             freq.put(mod, freq.getOrDefault(mod, 0) + 1);
         }
