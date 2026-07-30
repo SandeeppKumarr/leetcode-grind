6bcbd7428e26f7f0c9454931ebc3e20d883a7592
Sandeep Kumar <155826525+SandeeppKumarr@users.noreply.github.com>
2026-07-30 12:55:52 +0530

    Added a solution for Interval List Intersections

---
+++ Medium/986-interval-list-intersections.java
@@ -0,0 +1,34 @@
+/**
+ * LeetCode #986: Interval List Intersections
+ * Difficulty: Medium
+ * Language: Java
+ * Date: 2026-07-30T07:25:50.661Z
+ */
+
+class Solution {
+    public int[][] intervalIntersection(int[][] firstList, int[][] secondList) {
+        int i = 0, j = 0;
+        List<int[]> res = new ArrayList<>();
+        while(i < firstList.length && j < secondList.length){
+            if(firstList[i][0] < secondList[j][0]){
+                if(firstList[i][1] >= secondList[j][0]){
+                    int[] overlap = new int[2];
+                    overlap[0] = Math.max(firstList[i][0], secondList[j][0]);
+                    overlap[1] = Math.min(firstList[i][1], secondList[j][1]);
+                    res.add(overlap);
+                }
+            }
+            else{
+                if(secondList[j][1] >= firstList[i][0]){
+                    int[] overlap = new int[2];
+                    overlap[0] = Math.max(firstList[i][0], secondList[j][0]);
+                    overlap[1] = Math.min(firstList[i][1], secondList[j][1]);
+                    res.add(overlap);
+                }
+            }
+            if(firstList[i][1] > secondList[j][1]) j++;
+            else i++;
+        }
+        return res.toArray(new int[res.size()][]);
+    }
+}
