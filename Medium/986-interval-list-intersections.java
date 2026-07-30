30a8d09999e8325c4694c2199971d10de836f400
Sandeep Kumar <155826525+SandeeppKumarr@users.noreply.github.com>
2026-07-30 12:59:10 +0530

    Added a solution for Interval List Intersections

--- Medium/986-interval-list-intersections.java
+++ Medium/986-interval-list-intersections.java
@@ -2,33 +2,50 @@
  * LeetCode #986: Interval List Intersections
  * Difficulty: Medium
  * Language: Java
- * Date: 2026-07-30T07:25:56.981Z
+ * Date: 2026-07-30T07:29:09.343Z
  */
 
 class Solution {
     public int[][] intervalIntersection(int[][] firstList, int[][] secondList) {
         int i = 0, j = 0;
         List<int[]> res = new ArrayList<>();
-        while(i < firstList.length && j < secondList.length){
-            if(firstList[i][0] < secondList[j][0]){
-                if(firstList[i][1] >= secondList[j][0]){
-                    int[] overlap = new int[2];
-                    overlap[0] = Math.max(firstList[i][0], secondList[j][0]);
-                    overlap[1] = Math.min(firstList[i][1], secondList[j][1]);
-                    res.add(overlap);
-                }
+        // while(i < firstList.length && j < secondList.length){
+        //     if(firstList[i][0] < secondList[j][0]){
+        //         if(firstList[i][1] >= secondList[j][0]){
+        //             int[] overlap = new int[2];
+        //             overlap[0] = Math.max(firstList[i][0], secondList[j][0]);
+        //             overlap[1] = Math.min(firstList[i][1], secondList[j][1]);
+        //             res.add(overlap);
+        //         }
+        //     }
+        //     else{
+        //         if(secondList[j][1] >= firstList[i][0]){
+        //             int[] overlap = new int[2];
+        //             overlap[0] = Math.max(firstList[i][0], secondList[j][0]);
+        //             overlap[1] = Math.min(firstList[i][1], secondList[j][1]);
+        //             res.add(overlap);
+        //         }
+        //     }
+        //     if(firstList[i][1] > secondList[j][1]) j++;
+        //     else i++;
+        // }
+        while (i < firstList.length && j < secondList.length) {
+            int start = Math.max(firstList[i][0], secondList[j][0]);
+            int end = Math.min(firstList[i][1], secondList[j][1]);
+
+            // If the intervals overlap
+            if (start <= end) {
+                res.add(new int[]{start, end});
             }
-            else{
-                if(secondList[j][1] >= firstList[i][0]){
-                    int[] overlap = new int[2];
-                    overlap[0] = Math.max(firstList[i][0], secondList[j][0]);
-                    overlap[1] = Math.min(firstList[i][1], secondList[j][1]);
-                    res.add(overlap);
-                }
+
+            // Move the pointer whose interval ends first
+            if (firstList[i][1] < secondList[j][1]) {
+                i++;
+            } else {
+                j++;
             }
-            if(firstList[i][1] > secondList[j][1]) j++;
-            else i++;
         }
         return res.toArray(new int[res.size()][]);
+
     }
 }
