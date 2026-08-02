718e6dafbc1af23947ded81bd6469922519fd37a
Sandeep Kumar <155826525+SandeeppKumarr@users.noreply.github.com>
2026-08-02 23:33:41 +0530

    Added a solution for Stone Game

---
+++ Medium/877-stone-game.java
@@ -0,0 +1,12 @@
+/**
+ * LeetCode #877: Stone Game
+ * Difficulty: Medium
+ * Language: Java
+ * Date: 2026-08-02T18:03:37.375Z
+ */
+
+class Solution {
+    public boolean stoneGame(int[] piles) {
+        return true;
+    }
+}
