a4fd80beaa8f955303d7ae2bb5bccd0cc914e5be
Sandeep Kumar <155826525+SandeeppKumarr@users.noreply.github.com>
2026-08-04 17:14:26 +0530

    Added a solution for Maximum Number of Balloons

---
+++ Easy/1189-maximum-number-of-balloons.java
@@ -0,0 +1,22 @@
+/**
+ * LeetCode #1189: Maximum Number of Balloons
+ * Difficulty: Easy
+ * Language: Java
+ * Date: 2026-08-04T11:44:26.545Z
+ */
+
+class Solution {
+    public int maxNumberOfBalloons(String text) {
+        int[] freq = new int[26];
+        for (int i = 0; i < text.length(); i++) {
+            freq[text.charAt(i) - 'a']++;
+        }
+        int result = Integer.MAX_VALUE;
+        result = Math.min(result, freq['b' - 'a']);
+        result = Math.min(result, freq['a' - 'a']);
+        result = Math.min(result, freq['l' - 'a'] / 2);
+        result = Math.min(result, freq['o' - 'a'] / 2);
+        result = Math.min(result, freq['n' - 'a']);
+        return result;
+    }  
+}
