a1bde3d02310fb191a178d675680117ba5a7bf07
Sandeep Kumar <155826525+SandeeppKumarr@users.noreply.github.com>
2026-08-04 16:52:27 +0530

    Added a solution for Simplify Path

---
+++ Medium/71-simplify-path.java
@@ -0,0 +1,21 @@
+/**
+ * LeetCode #71: Simplify Path
+ * Difficulty: Medium
+ * Language: Java
+ * Date: 2026-08-04T11:22:26.857Z
+ */
+
+class Solution {
+    public String simplifyPath(String path) {
+        Stack<String> stack = new Stack<>();
+        String[] direc = path.split("/");
+        for(String dir : direc){
+            if(dir.equals(".") || dir.isEmpty()) continue;
+            else if(dir.equals("..")){
+                if(!stack.isEmpty()) stack.pop();
+            }
+            else stack.push(dir);
+        }
+        return "/" + String.join("/", stack);
+    }
+}
