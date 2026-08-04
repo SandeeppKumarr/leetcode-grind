ec7a08d9f1562b828c4f201fca685b66382cea29
Sandeep Kumar <155826525+SandeeppKumarr@users.noreply.github.com>
2026-08-04 15:56:08 +0530

    Added a solution for Daily Temperatures

---
+++ Medium/739-daily-temperatures.java
@@ -0,0 +1,25 @@
+/**
+ * LeetCode #739: Daily Temperatures
+ * Difficulty: Medium
+ * Language: Java
+ * Date: 2026-08-04T10:26:09.144Z
+ */
+
+class Solution {
+    public int[] dailyTemperatures(int[] temperatures) {
+        int n = temperatures.length;
+        Stack<Integer> stack = new Stack<>(); //stores index
+        int[] result = new int[n];
+        for(int i = n - 1; i >= 0; i--){
+            while(!stack.isEmpty() && temperatures[stack.peek()] <= temperatures[i]){
+                stack.pop();
+            }
+            if(!stack.isEmpty()){
+                result[i] = stack.peek() - i;
+            }
+            else result[i] = 0;
+            stack.push(i);
+        }
+        return result;
+    }
+}
