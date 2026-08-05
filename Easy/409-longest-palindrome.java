4844ad869335a4b5ab42813f5f0c825e107e159a
Sandeep Kumar <155826525+SandeeppKumarr@users.noreply.github.com>
2026-08-05 11:15:45 +0530

    Added a solution for Longest Palindrome

--- Easy/409-longest-palindrome.java
+++ Easy/409-longest-palindrome.java
@@ -2,7 +2,7 @@
  * LeetCode #409: Longest Palindrome
  * Difficulty: Easy
  * Language: Java
- * Date: 2026-08-05T05:44:09.021Z
+ * Date: 2026-08-05T05:45:45.957Z
  */
 
 class Solution {
@@ -16,7 +16,7 @@ class Solution {
         int length = 0;
         for (int count : freq) {
             length += (count / 2) * 2; //for even we add count, and for odd it will become count - 1
-            if (length % 2 == 0 && count % 2 == 1) {
+            if (length % 2 == 0 && count % 2 == 1) { //basically if we have not yet added middle character & current count can spare 1
                 length++;
             }
         }
