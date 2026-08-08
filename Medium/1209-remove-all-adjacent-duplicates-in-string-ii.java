0499f50f6ce6a1dff7e101c855a067fc0e178a8b
Sandeep Kumar <155826525+SandeeppKumarr@users.noreply.github.com>
2026-08-08 14:03:14 +0530

    Added a solution for Remove All Adjacent Duplicates in String II

--- Medium/1209-remove-all-adjacent-duplicates-in-string-ii.java
+++ Medium/1209-remove-all-adjacent-duplicates-in-string-ii.java
@@ -2,28 +2,54 @@
  * LeetCode #1209: Remove All Adjacent Duplicates in String II
  * Difficulty: Medium
  * Language: Java
- * Date: 2026-08-07T16:12:25.374Z
+ * Date: 2026-08-08T08:33:12.693Z
  */
 
 class Solution {
+
+    class Pair {
+        char ch;
+        int count;
+
+        Pair(char ch, int count) {
+            this.ch = ch;
+            this.count = count;
+        }
+    }
+
     public String removeDuplicates(String s, int k) {
-        int[] count = new int[s.length()];
-        StringBuilder sb = new StringBuilder();
-        for(char ch : s.toCharArray()){
-            int size = sb.length();
-            if(size > 0 && sb.charAt(size - 1) == ch){
-                count[size] = count[size - 1] + 1;
+
+        Stack<Pair> st = new Stack<>();
+
+        for (int i = 0; i < s.length(); i++) {
+
+            char c = s.charAt(i);
+
+            // Stack is empty OR top character is different
+            if (st.isEmpty() || st.peek().ch != c) {
+                st.push(new Pair(c, 1));
             }
-            else count[size] = 1;
-            if(count[size] == k){
-                for(int i = 0; i < k - 1; i++){
-                    sb.deleteCharAt(size - i - 1);
-                    count[size] = 0;
+
+            // Same character as top
+            else {
+                st.peek().count++;
+
+                // We have k duplicates
+                if (st.peek().count == k) {
+                    st.pop();
                 }
             }
-            else sb.append(ch);
-            //System.out.println(sb.toString());
         }
-        return sb.toString();
+
+        // Build answer from stack
+        StringBuilder ans = new StringBuilder();
+
+        for (Pair p : st) {
+            for (int i = 0; i < p.count; i++) {
+                ans.append(p.ch);
+            }
+        }
+
+        return ans.toString();
     }
 }
