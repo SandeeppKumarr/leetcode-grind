91b5cb1734b28977c643def2da3deaaa455d2d5a
Sandeep Kumar <155826525+SandeeppKumarr@users.noreply.github.com>
2026-08-09 14:33:58 +0530

    Added a solution for Koko Eating Bananas

---
+++ Medium/875-koko-eating-bananas.java
@@ -0,0 +1,33 @@
+/**
+ * LeetCode #875: Koko Eating Bananas
+ * Difficulty: Medium
+ * Language: Java
+ * Date: 2026-08-09T09:03:55.854Z
+ */
+
+class Solution {
+    public int minEatingSpeed(int[] piles, int h) {
+        int low = 1;
+        int high = Integer.MIN_VALUE;
+        int result = high;
+        for(int pile : piles){
+            high = Math.max(high, pile);
+        }
+
+        while(low <= high){
+            int mid = low + (high - low) / 2;
+
+            int hours = 0;
+            for(int pile : piles){
+                hours += pile / mid;
+                if(pile % mid != 0) hours++;
+            }
+            if(hours <= h){
+                result = mid;
+                high = mid - 1;
+            }
+            else low = mid + 1;
+        }
+        return result;
+    }
+}
